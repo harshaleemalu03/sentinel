5b54cbc0f39316036b8a22bac5d89ebbf4b04545
Harshalee Malu <harshaleemalu03@gmail.com>
2026-09-05 21:56:57 +0530

    Fix Vercel production build

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const rootDir = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  // output: 'standalone',
 
   // Standalone output for clean cloud and serverless deployment
   images: {
